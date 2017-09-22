e5cb2f5b73c8a911fe90788d904657c32f70f19a
mac010 <mac010@sinruiscute.local>
2017-09-22 12:10:18 +0800

    finish TDD

--- test/loginTest.js
+++ test/loginTest.js
@@ -1,8 +1,15 @@
-var assert = require('assert');
-describe, function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+var assert = require('chai').assert
+describe('otrak', function () {
+    var M = require('./manager')
+    describe('login', function() {
+        it('login(ccc, 1111)===undefined', function () {
+            assert.isUndefined(M.login('ccc', '1111'))
+        })
+        it('login(ccc, 1234567).id=ccc', function () {
+            assert.equal('ccc', M.login('ccc', '1234567').id)
+        }) 
+        it('login(leo, 1234567)====undefined', function () {
+            assert.isUndefined(M.login('leo', '1234567'))
+        }) 
+  })
+})
